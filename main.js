8c4308f2fd215f38ba4de34f75835cd5cb835702
Areesha23 <areesha.alamgir@torontomu.ca>
2024-02-26 13:01:27 -0500

    commit

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+
+
 document.getElementById("EnterButton").onclick=function(){
     var name=prompt("Enter your name here: ");
     if (name!null && name!=""){
     document.getElementById("myname").innerHTML="Welcome" + name;
 }
 };
+
 
 img=document.getElementById("meimg");
 function bigimg() {
