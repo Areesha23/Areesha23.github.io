99d36455d5555289fadef3e3db06b12ecee5059c
Areesha23 <areesha.alamgir@torontomu.ca>
2024-03-11 21:09:57 -0400

    commit

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function DDFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
 
-// show dropdown
-function showDropdown(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
-    dropdown.style.display = 'block';
-}
+  var slider = document.getElementById("myRange");
+  var output = document.getElementById("val");
+  output.innerHTML=slider.value;
+  
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
 
-// hide dropdown
-function hideDropdown(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
-    dropdown.style.display = 'none';}
+
+ 
+  window.addEventListener('load', function() {
+  document.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault(); // Prevent the default form submission 
