fbc861d499f2da970fd11754925dafc06b787c40
Areesha23 <areesha.alamgir@torontomu.ca>
2024-03-11 21:31:55 -0400

    commit

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function DDFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
     }
   }
+}
+
+
+
 
   var slider = document.getElementById("myRange");
   var output = document.getElementById("val");
@@ -22,9 +25,3 @@ function DDFunction() {
   slider.oninput = function() {
     output.innerHTML = this.value;
   }
-
-
- 
-  window.addEventListener('load', function() {
-  document.querySelector('form').addEventListener('submit', function(e) {
-        e.preventDefault(); // Prevent the default form submission 
